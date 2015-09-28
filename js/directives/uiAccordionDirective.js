3d51be1597d49856d966945cf2b41ad5c477d9c7
Bryan Marvila <bryan7c@gmail.com>
2015-09-28 17:06:06 -0300

    accordions

--- js/directives/uiAccordionDirective.js
+++ js/directives/uiAccordionDirective.js
@@ -1,38 +1,35 @@
-angular.module("listaTelefonica").directive("uiDate",function($filter){
+angular.module("listaTelefonica").directive("uiAccordions",function(){
 	return {
-		require: "ngModel",
-		link: function(scope, element, attrs, ctrl){
-			var _formatDate = function(date){
-				date = date.replace(/[^0-9]+/g, "");
-				if(date.length > 2){
-					date = date.substring(0,2) + "/" + date.substring(2);
-				};
-				if(date.length > 5){
-					date = date.substring(0,5) + "/" + date.substring(5,9);
-				};
-				return date;
+		controller: function($scope, $element, $attrs){
+			var accordions = [];
+			this.registerAccordions = function(accordion){
+				accordions.push(accordion);
+				console.log(accordions);
 			};
 
-			//captura de evento do elemento
-			element.bind("keyup", function(){
-				ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
-				ctrl.$render();
-			});
+			this.closeAllAccordions = function(){
+				accordions.forEach(function(_accordion){
+					_accordion.isOpened = false;
+				});
+			};
+		}
+	};
+});
 
-			//interceptando valor antes de enviar para o scope
-			ctrl.$parsers.push(function (value) {
-				if (value.length === 10) {
-					var pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
-					return new Date(value.replace(pattern,'$3-$2-$1')).getTime();
-					// var dateArray = value.split("/");
-					// return new Date(dateArray[2], dateArray[1]-1, dateArray[0]).getTime();
-				}
-			});
-
-			//interceptando valor antes de printar na tala pelo ng-model
-			ctrl.$formatters.push(function (value) {
-				return $filter("date")(value, "dd/MM/yyyy");
-			});
+angular.module("listaTelefonica").directive("uiAccordion",function(){
+	return {
+		templateUrl: "view/accordion.html",
+		transclude: true,
+		scope: {
+			title: "@"
+		},
+		require: "^uiAccordions",
+		link: function(scope, element, attrs, ctrl){
+			ctrl.registerAccordions(scope);
+			scope.open = function(){
+				ctrl.closeAllAccordions();
+				scope.isOpened = true;
+			}
 		}
 	}
 });
