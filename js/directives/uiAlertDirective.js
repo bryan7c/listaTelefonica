2d1ff709fb3ca407776a998b85db0ef81b10b25a
Bryan Marvila <bryan7c@gmail.com>
2015-10-02 09:44:05 -0300

    Merge branch 'filtro'

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -11,6 +11,13 @@ angular.module("listaTelefonica").directive("uiAlert",function(){
 		scope: {
 			topic: "@title" //apenas o @ pois eles tem o mesmo nome
 		},
-		transclude: true
+		transclude: true, //enviar o conteúdo de dentro da custon tag para dentro do template
+		require: '', //uma diretiva requerida, com este campo setado é possivel utilizar o controller deta diretiva dentro do link
+		/*	param (scope, element, attr, ctrl, transclude)
+		 *	seta function para iniciacao da diretiva (capturar bind e setar coisas do scope)
+		 */
+		link: '',
+		controller: '', //controller que será utilizado dentro do componente gerado pela diretiva
+		controllerAs: '', //apelido para o controller, boa pratica para evitar o scope
 	}
 });
