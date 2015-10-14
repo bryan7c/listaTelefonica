e44e21535cbc5d2eb335bab764fc315c729c0fc2
Bryan Marvila <bryan7c@gmail.com>
2015-10-14 09:45:38 -0300

    Capitulo 13 - Modularizando o projeto

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -15,10 +15,6 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 	var carregarOperadoras = function(){
 		operadorasAPI.getOperadoras().success(function(data, status){
 			$scope.operadoras = data;
-
-            var outra;
-            var master altaracao;
-
 		}).error(function(data, status){
 			$scope.erro = "Não foi possível carregar os dados!";
 		});
